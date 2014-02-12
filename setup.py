d153ed0c444d9b50a4479b968505a2f26c5f978d
Joe Frew <joefrew95@gmail.com>
2014-02-12 18:03:22 +0000

    corrected dependencies so pygame installs automatically

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 from setuptools import setup
 
-dependencies = ["pygame"]
-
 setup(name='grapy',
       version='0.1',
       description='A simple force directed graph library',
@@ -9,6 +7,6 @@ setup(name='grapy',
       author='Joe Frew',
       author_email='2089249F@students.gla.ac.uk',
       license='MIT',
-      packages=['grapy'],
+      install_requires=['pygame>=1.9.1'],
       dependencies = dependencies,
       zip_safe=False)
